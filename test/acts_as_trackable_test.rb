9bf2094fdf3823d1ed855a556a1c2d42d4954709
Jake <github@jake.otherinbox.com>
2010-03-12 16:56:37 -0800

    Initial tests

--- test/acts_as_trackable_test.rb
+++ test/acts_as_trackable_test.rb
@@ -1,8 +1,55 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/test_helper'
 
 class ActsAsTrackableTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+
+	def setup
+		setup_db
+	end
+
+	def teardown
+		teardown_db
+	end
+
+	test "should create book" do
+		assert_difference 'Book.count' do
+			book = create_book
+			assert !book.new_record?, 
+				"#{book.errors.full_messages.to_sentence}"
+		end
+	end
+
+	test "should require tracking_number" do
+		assert_no_difference 'Book.count' do
+			book = create_book(:tracking_number => nil)
+			assert book.errors.on(:tracking_number)
+		end
+	end
+
+	test "should require 3 char tracking_number" do
+		assert_no_difference 'Book.count' do
+			book = create_book(:tracking_number => 'Hi')
+			assert book.errors.on(:tracking_number)
+		end
+	end
+
+	test "should require unique tracking_number" do
+		b = create_book
+		assert_no_difference 'Book.count' do
+			book = create_book(:tracking_number => b.tracking_number)
+			assert book.errors.on(:tracking_number)
+		end
+	end
+
+
+
+protected
+
+	def create_book(options = {})
+		record = Book.new({
+			:tracking_number => '123'
+		}.merge(options))
+		record.save
+		record
+	end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,47 @@
+require 'test/unit'	#	<-- needed but not added by rails generator
 require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
+require 'active_record'
+require File.dirname(__FILE__) + '/../init'
+
+
+ActiveRecord::Base.establish_connection(
+	:adapter => "sqlite3", :database => ":memory:")
+
+def setup_db
+	ActiveRecord::Schema.define(:version => 1) do
+		create_table :tracks do |t|
+			t.references :trackable, :polymorphic => true
+			t.string :location
+			t.string :name
+			t.datetime :time
+			t.timestamps
+		end
+		add_index :tracks, [:trackable_id, :trackable_type, :time], 
+			:unique => true
+		create_table :books do |t|
+			t.string :title
+			t.string :tracking_number
+		end
+	end
+end
+
+def teardown_db
+	ActiveRecord::Base.connection.tables.each do |table|
+		ActiveRecord::Base.connection.drop_table(table)
+	end
+end
+
+class Track < ActiveRecord::Base
+end
+class Package < ActiveRecord::Base
+	acts_as_trackable
+end
+class Book < ActiveRecord::Base
+	acts_as_trackable
+end
+
+class ActiveSupport::TestCase
+
+end
