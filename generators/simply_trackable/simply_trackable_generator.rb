85cee6c34995ac4f368279b49981bf62482413e4
Jake <github@jake.otherinbox.com>
2010-11-05 13:09:35 -0700

    Version bump to 1.2.6

--- generators/simply_trackable/simply_trackable_generator.rb
+++ generators/simply_trackable/simply_trackable_generator.rb
@@ -2,16 +2,20 @@ class SimplyTrackableGenerator < Rails::Generator::Base
 
 	def manifest
 		record do |m|
+			m.directory('config/autotest')
+			m.file('autotest_simply_trackable.rb', 'config/autotest/simply_trackable.rb')
+			m.directory('lib/tasks')
+			m.file('simply_trackable.rake', 'lib/tasks/simply_trackable.rake')
 
-			File.open('Rakefile','a'){|f| 
-				f.puts "#	From `script/generate simply_trackable` ..."
-				f.puts "require 'simply_trackable/test_tasks'"
-			}
-
-			File.open('.autotest','a'){|f| 
-				f.puts "#	From `script/generate simply_trackable` ..."
-				f.puts "require 'simply_trackable/autotest'"
-			}
+#			File.open('Rakefile','a'){|f| 
+#				f.puts "#	From `script/generate simply_trackable` ..."
+#				f.puts "require 'simply_trackable/test_tasks'"
+#			}
+#
+#			File.open('.autotest','a'){|f| 
+#				f.puts "#	From `script/generate simply_trackable` ..."
+#				f.puts "require 'simply_trackable/autotest'"
+#			}
 
 			# m.directory "lib"
 			# m.template 'README', "README"
