779e2de78df27921a0eb2c60cfeb9b3074ce7342
Jake <github@jakewendt.com>
2011-02-11 10:57:24 -0800

    Updated rails test env to 2.3.11 and cleaned up environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -11,15 +11,13 @@ Rails::Initializer.run do |config|
 #	config.gem "thoughtbot-factory_girl",
 #		:lib => "factory_girl"
 
-	config.gem "jakewendt-simply_testable"	#,
-#		:lib => "simply_testable"
+	config.gem "jakewendt-simply_testable"
+	config.gem "jakewendt-html_test"
 
 	config.plugin_paths = [
-		File.expand_path(File.join(File.dirname(__FILE__),'../..')),
-		File.expand_path(File.join(File.dirname(__FILE__),'../../..','peter'))
+		File.expand_path(File.join(File.dirname(__FILE__),'../..'))
 	]
-	config.plugins = [ :simply_trackable, 
-		:html_test, :html_test_extension ]
+	config.plugins = [ :simply_trackable ]
 
 	config.frameworks -= [:active_resource]
 
