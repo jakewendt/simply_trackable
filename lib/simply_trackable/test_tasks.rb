9c34068b19e279e6b90412befdf219f0f8215831
Jake <github@jakewendt.com>
2011-06-01 15:18:39 -0700

    Removed functional from gem tests as aren't any.

--- lib/simply_trackable/test_tasks.rb
+++ lib/simply_trackable/test_tasks.rb
@@ -33,6 +33,19 @@ Rake::Task['test:units'].prerequisites.unshift(
 @gem_test_dirs ||= []
 @gem_test_dirs << File.expand_path(File.join(File.dirname(__FILE__),
 	'/../../test/unit/trackable/'))
-@gem_test_dirs << File.expand_path(File.join(File.dirname(__FILE__),
-	'/../../test/functional/trackable/'))
+
+#
+#	No functional tests so...
+#
+#/Library/Ruby/Gems/1.8/gems/rcov-0.9.9/bin/rcov:516:in `load': no such file to load -- /Library/Ruby/Gems/1.8/gems/jakewendt-simply_trackable-1.3.5/test/functional/trackable/*_test.rb (LoadError)
+#	from /Library/Ruby/Gems/1.8/gems/rcov-0.9.9/bin/rcov:516
+#	from /usr/bin/rcov:19:in `load'
+#	from /usr/bin/rcov:19
+#
+#	Actually, I think the error is because the directory isn't there
+#	because there aren't any files in the gem in that directory.
+#	Perhaps I could try to force it.  Until then, comment this line out.
+#
+#@gem_test_dirs << File.expand_path(File.join(File.dirname(__FILE__),
+#	'/../../test/functional/trackable/'))
 
